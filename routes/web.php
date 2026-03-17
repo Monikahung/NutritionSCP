061a59a9cb91003741c6752f54a42f53307c281c
Monikahung <monikahung580@gmail.com>
2026-03-17 20:33:44 +0700

    Fix and update route

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,44 @@
 <?php
 
 use App\Http\Controllers\Admin\AboutUsController;
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\DemoController;
 use App\Http\Controllers\Admin\HomeAdminController;
 use App\Http\Controllers\Admin\NutritionProductController;
+use App\Http\Controllers\Public\HomePublicController;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/test-api', function () {
     $res = Http::get('https://world.openfoodfacts.org/cgi/search.pl?search_terms=minuman&json=true&page_size=24');
+
     return $res->json();
 });
-Route::get('/', function () {
-    return view('login');
-});
 
-Route::get('/admin', [HomeAdminController::class, 'index'])->name('dashboardadmin');
+// Route homepublic menampilkan home public
+Route::get(
+    '/',
+    [HomePublicController::class,
+        'HomePublic'])
+    ->name('homepublic');
 
-Route::get('/admin/aboutus', [AboutUsController::class, 'index'])->name('aboutus');
-Route::get('/admin/produk', [NutritionProductController::class, 'index'])->name('products.index');
-Route::get('/admin/produk/{code}', [NutritionProductController::class, 'show'])->name('products.show');
+Route::get(
+    '/admin',
+    [HomeAdminController::class,
+        'index'])
+    ->name('dashboardadmin');
 
+Route::get(
+    '/admin/aboutus',
+    [AboutUsController::class,
+        'index'])
+    ->name('aboutus');
 
-Route::get('/public', [HomePublicController::class, 'HomePublic'])->name('homepublic');
+Route::get(
+    '/admin/produk',
+    [NutritionProductController::class,
+        'index'])
+    ->name('products.index');
+
+Route::get(
+    '/admin/produk/{code}',
+    [NutritionProductController::class,
+        'show'])
+    ->name('products.show');
