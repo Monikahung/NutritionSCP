09e0929df36d56cef6f51e21a0952701db94cfb7
Nessalyn <149561544+NessLM@users.noreply.github.com>
2026-03-17 18:45:01 +0700

    Push

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use App\Http\Controllers\Admin\AboutUsController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\DemoController;
 use App\Http\Controllers\Admin\HomeAdminController;
 use App\Http\Controllers\Admin\NutritionProductController;
 use Illuminate\Support\Facades\Http;
