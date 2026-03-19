039e47cf16473b6b13f8baef9db9cb1ef89778b0
Vanessa Angelika <vanessaangelika22@gmail.com>
2026-03-20 01:50:07 +0700

    update route

--- routes/web.php
+++ routes/web.php
@@ -7,13 +7,12 @@ use App\Http\Controllers\Admin\NutritionProductController;
 use App\Http\Controllers\Public\HomePublicController;
 
 // Route homepublic menampilkan home public
-Route::get('/public',[HomePublicController::class,'HomePublic'])->name('homepublic');
-
+Route::get('/', [HomePublicController::class, 'HomePublic'])->name('homepublic');
 
 Route::get('/admin/api/products', [NutritionProductController::class, 'api']);
-Route::get('/admin',[HomeAdminController::class,'index'])->name('dashboardadmin');
-Route::get('/admin/aboutus',[AboutUsController::class,'index'])->name('aboutus');
+Route::get('/admin', [HomeAdminController::class, 'index'])->name('dashboardadmin');
+Route::get('/admin/aboutus', [AboutUsController::class, 'index'])->name('aboutus');
 
-Route::get('/admin/produk',[NutritionProductController::class,'index'])->name('products.index');
+Route::get('/admin/produk', [NutritionProductController::class, 'index'])->name('products.index');
 
-Route::get('/admin/produk/{code}',[NutritionProductController::class,'show'])->name('products.show');
+Route::get('/admin/produk/{code}', [NutritionProductController::class, 'show'])->name('products.show');
